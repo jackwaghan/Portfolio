faacde356f6681c5bdd2482130c11aabd43fd786
Jack Waghan <jackwaghan@gmail.com>
2025-01-29 02:18:18 +0530

    added Lenis smooth scroll

---
+++ src/app/Components/LenisProvider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useLenis } from "../../utils/hook";
+
+export default function LenisProvider() {
+  useLenis();
+  return null;
+}
--- src/utils/hook.ts
+++ src/utils/hook.ts
@@ -1,5 +1,6 @@
+"use client";
 import { useEffect, useState } from "react";
-
+import Lenis from "@studio-freight/lenis";
 export function useWindowSize() {
   const [windowSize, setWindowSize] = useState({
     width: 0,
@@ -17,4 +18,27 @@ export function useWindowSize() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
   return windowSize;
+}
+
+export function useLenis() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 4,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return null; // This component doesn't render anything
 }
